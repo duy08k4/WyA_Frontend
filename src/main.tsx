01eeadc16617d018cdf55686eb09b395d9f67291
duyo8k4 <duytran.290804@gmail.com>
2025-04-22 19:57:25 +0700

    Add Redux to Project

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 
 import { ToastProvider } from './components/toastMessage/toast';
 import { SpinnerProvider } from './components/spinner/spinner';
@@ -8,9 +10,11 @@ import { SpinnerProvider } from './components/spinner/spinner';
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
-  <SpinnerProvider>
-    <ToastProvider>
-      <App />
-    </ToastProvider>
-  </SpinnerProvider>
+  <Provider store={store}>
+    <SpinnerProvider>
+      <ToastProvider>
+        <App />
+      </ToastProvider>
+    </SpinnerProvider>
+  </Provider>
 );
