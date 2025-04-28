08a280b4b8982a0664f9d943a1c8b289aa351227
duyo8k4 <duytran.290804@gmail.com>
2025-04-28 13:21:37 +0700

    fix(config): fixed

--- src/config/firebaseSDK.ts
+++ src/config/firebaseSDK.ts
@@ -2,17 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Config này lấy trong Firebase Console (Project Settings -> General -> Your apps)
-const firebaseConfig = {
-    apiKey: "AIzaSyBM6ZMQw6h705Stg5okPQuwaWdXilCEjJc",
-    authDomain: "wya-where-you-at.firebaseapp.com",
-    projectId: "wya-where-you-at",
-    storageBucket: "wya-where-you-at.firebasestorage.app",
-    messagingSenderId: "59746391501",
-    appId: "1:59746391501:web:b07cdf4dc0fe4bc16ae084",
-    measurementId: "G-785D7SNY5W"
-};
-
+const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
 // Khởi tạo Firebase
 const app = initializeApp(firebaseConfig);
 
