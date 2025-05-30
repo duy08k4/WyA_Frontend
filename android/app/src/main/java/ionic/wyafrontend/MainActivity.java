2749d33d7012fd98e0962338faeb0cf4cc3bd276
duyo8k4 <duytran.290804@gmail.com>
2025-05-30 21:17:43 +0700

    _

--- android/app/src/main/java/ionic/wyafrontend/MainActivity.java
+++ android/app/src/main/java/ionic/wyafrontend/MainActivity.java
@@ -2,12 +2,4 @@ package ionic.wyafrontend;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        WebSettings settings = this.getBridge().getWebView().getSettings();
-        settings.setTextZoom(100);
-}
-
-}
+public class MainActivity extends BridgeActivity {}
